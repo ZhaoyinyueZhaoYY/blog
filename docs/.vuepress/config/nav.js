f89935ac20075184f997a8513b0af45dac65a06e
ZhaoYinYue <zhaoyinyue@hikvision.com.cn>
2020-11-17 15:05:04 +0800

    部署

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -4,9 +4,10 @@ module.exports = [
     text: '工作',
     link: '/work/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      // {text: '前端文章', items: [
-      //   {text: 'JavaScript', link: '/pages/8143cc480faf9a11/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
-      //   {text: 'Vue', link: '/pages/802a1ca6f7b71c59/'},
+      {text: '杂七杂八', link: '/pages/c1bdc2/'},
+      // {text: '前端', items: [
+      //   {text: 'JavaScript', link: '/pages/c1bdc2/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
+      //   // {text: 'Vue', link: '/pages/802a1ca6f7b71c59/'},
       // ]},
       // {text: '学习笔记', items:[
       //   {text: '《JavaScript教程》笔记', link: '/note/javascript/'},
@@ -22,7 +23,7 @@ module.exports = [
     text: '生活',
     link: '/life/',
     items: [
-      // {text: '技术文档', link: '/pages/9a7ee40fc232253e/'},
+      {text: '生活', link: '/pages/89bb30/'},
       // {text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/'},
       // {text: 'Nodejs', link: '/pages/117708e0af7f0bd9/'},
       // {text: '博客搭建', link: '/pages/41f87d890d0a02af/'},
